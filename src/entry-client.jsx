b371179df2039de36b5a3665221903122d20c50f
artistrea <apiravesi@gmail.com>
2023-02-28 18:01:10 -0300

    added tailwind config

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import "./index.css";
 
 ReactDOM.hydrateRoot(document.getElementById("app"), <App />);
 console.log("hydrated");
